0f320e09548fa15f6897e2970b2d599b72068895
Kim Do <kim.h.do.seven@gmail.com>
2019-04-09 17:14:28 -0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cs151_project
+
+1. Open your terminal and type in commands:
+    * [ ] `$ git clone https://github.com/kimdo7/cs151_project`
+    * [ ] `$ cd cs151`  
+    * [ ] `$ npm install` 
+    * [ ] `$ npm install -g nodemon`
+    * [ ] `$ nodemon server.js` or `$ node server.js` 
+    * [ ] `open your browser on localhost:6789`
---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+    console.log('client request URL: ', request.url);
+    if (request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(contents);
+            response.end();
+        });
+    }else if (request.url === "/static/style.css") {
+        fs.readFile('./static/style.css', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'text/css' });
+            response.write(contents);
+            response.end();
+        });
+    }else if (request.url === "/static/script.js") {
+        fs.readFile('./static/script.js', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'text/javascript' });
+            response.write(contents);
+            response.end();
+        });
+    }else {
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
