684338e85d55ed98afc677753a2289f216f77833
DeadlyPho <jasontang.exe@gmail.com>
2019-04-30 00:01:33 -0700

    Added Return Edge

--- static/JasonScripts/script.js
+++ static/JasonScripts/script.js
@@ -770,8 +770,8 @@ class CallEdge extends SegmentedLineEdge
    {
       let a = [];
       let n = this.getEnd();
-      start = this.getStart().getBounds();
-      end = n.getBounds();
+      let start = this.getStart().getBounds();
+      let end = n.getBounds();
       
       if (n instanceof CallNode &&(n.getImplicitParameter() ==(this.getStart()).getImplicitParameter()))
       {
@@ -805,8 +805,42 @@ class CallEdge extends SegmentedLineEdge
       return a;
    }
 }
+/**
+   An edge that joins two call nodes.
+*/
+class ReturnEdge extends SegmentedLineEdge
+{
+   constructor()
+   {
+	super();
+      this.setEndArrowHead(ArrowHead.V);
+      this.setLineStyle(LineStyle.DOTTED);
+   }
 
-
+   getPoints()
+   {
+      let a = [];
+      let n = this.getEnd();
+      let start = this.getStart().getBounds();
+      let end = this.getEnd().getBounds();
+      if (n instanceof PointNode) // show nicely in tool bar
+      {
+         a.push(new Point2D(end.getX(), end.getY()));
+         a.push(new Point2D(start.getMaxX(), end.getY()));
+      }      
+      else if (start.getCenterX() < end.getCenterX())
+      {
+         a.push(new Point2D(start.getMaxX(), start.getMaxY()));
+         a.push(new Point2D(end.getX(), start.getMaxY()));
+      }
+      else
+      {
+         a.push(new Point2D(start.getX(), start.getMaxY()));
+         a.push(new Point2D(end.getMaxX(), start.getMaxY()));
+      }
+      return a;
+   }
+}
 
 
 
@@ -814,7 +848,7 @@ class CallEdge extends SegmentedLineEdge
 
 document.addEventListener('DOMContentLoaded', function () {
 
-var edge=new CallEdge();
+var edge=new ReturnEdge();
 var p2=new Rectangle2D(100,400,40,40);
 var p1=new Rectangle2D(500,300,40,40);
 //edge.draw(p1,p2);
@@ -838,4 +872,5 @@ let testPoint=new Point2D(500,300);
 //var direction=new Direction(point1,point2);
 //console.log(direction.getX());
 console.log(edge.contains(testPoint));
+//console.log(edge.getPoints());
 });
